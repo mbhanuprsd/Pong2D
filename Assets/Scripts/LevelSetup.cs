c7b8a23b363552f1f5f82bbb6465f6a5a089bab3
Bhanu Prasad M <mbhanuprsd@gmail.com>
2022-04-23 03:21:57 +0530

    Spawn bricks based on number of layers and screen width

--- Assets/Scripts/LevelSetup.cs
+++ Assets/Scripts/LevelSetup.cs
@@ -1,10 +1,29 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelSetup : MonoBehaviour
 {
     [SerializeField]
     GameObject ball, topWall, leftWall, rightWall, player, brickPrefab;
+
+    private List<GameObject> bricks;
+
+    [SerializeField]
+    int numberOfLayers = 2;
+
+    private float ScreenWidth, ScreenHeight;
+
+    private void Start()
+    {
+        bricks = new List<GameObject>();
+        ScreenWidth = (float)(Camera.main.orthographicSize * 2.0f * Screen.width / Screen.height);
+        ScreenHeight = (float)(Camera.main.orthographicSize * 2.0f);
+
+        SpawnBricks();
+
+        ToggleGameObjectsState(false);
+    }
 
     public void SpawnLevel()
     {
@@ -28,13 +47,17 @@ public class LevelSetup : MonoBehaviour
         leftWall.SetActive(active);
         rightWall.SetActive(active);
         player.SetActive(active);
+        foreach (GameObject brick in bricks)
+        {
+            brick.SetActive(active);
+        }
     }
 
     private void InitializeBall()
     {
         ball.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
         ball.GetComponent<Rigidbody2D>().WakeUp();
-        ball.GetComponent<Rigidbody2D>().AddForce(new Vector2(4, 4), ForceMode2D.Impulse);
+        ball.GetComponent<Rigidbody2D>().AddForce(new(4, 4), ForceMode2D.Impulse);
     }
 
     /// <summary>
@@ -46,23 +69,56 @@ public class LevelSetup : MonoBehaviour
         Vector3 RightWallPosition = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, Camera.main.nearClipPlane));
         Vector3 TopWallPosition = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1, Camera.main.nearClipPlane));
 
-        float ScreenWidth = (float)(Camera.main.orthographicSize * 2.0f * Screen.width / Screen.height);
-        float ScreenHeight = (float)(Camera.main.orthographicSize * 2.0f);
-
         // Spawn the wall boundaries with respect to screen position
         leftWall.transform.SetPositionAndRotation(LeftWallPosition, Quaternion.identity);
         rightWall.transform.SetPositionAndRotation(RightWallPosition, Quaternion.identity);
         topWall.transform.SetPositionAndRotation(TopWallPosition, Quaternion.Euler(0, 0, 90));
 
         // Make the boundaries change based on screen size
-        leftWall.transform.localScale = new Vector3(0.1f, ScreenHeight, 1);
-        rightWall.transform.localScale = new Vector3(0.1f, ScreenHeight, 1);
-        topWall.transform.localScale = new Vector3(0.1f, ScreenWidth, 1);
+        leftWall.transform.localScale = new(0.1f, ScreenHeight, 1);
+        rightWall.transform.localScale = new(0.1f, ScreenHeight, 1);
+        topWall.transform.localScale = new(0.1f, ScreenWidth, 1);
     }
 
     private void InitializePlayer()
     {
         Vector3 playerInitialPosition = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.1f, Camera.main.nearClipPlane));
         player.transform.SetPositionAndRotation(playerInitialPosition, Quaternion.Euler(0, 0, 90));
+        player.transform.localScale = new Vector3(ScreenWidth / 24.0f, ScreenWidth / 8.0f, 1);
+    }
+
+    /// <summary>
+    /// Spawn bricks based o number of layers and screen width
+    /// </summary>
+    private void SpawnBricks()
+    {
+        var blockWidth = 1 / 8.0f;
+        var blockHeight = 1 / 32.0f;
+        Vector3 brickScale = new(ScreenWidth/8.0f, ScreenHeight/32.0f, 1);
+        var blockPadding = 1 / 80.0f;
+        var i = 0;
+        var j = numberOfLayers;
+        while (j > 0)
+        {
+            var start = i * (blockWidth + blockPadding) + blockPadding;
+            var end = 1.0f - start - blockPadding;
+            var blockXCoord = start;
+            var blockYCoord = i * (blockHeight + blockPadding) + blockPadding;
+            j--;
+            while (end > blockXCoord + blockWidth)
+            {
+                Vector3 brickPosition = Camera.main.ViewportToWorldPoint(
+                    new Vector3(
+                       1 - blockXCoord - blockWidth/2.0f -blockPadding,
+                       0.9f - blockHeight - blockYCoord,
+                        Camera.main.nearClipPlane)
+                    );
+                GameObject newBrick = Instantiate(brickPrefab, brickPosition, Quaternion.identity);
+                newBrick.transform.localScale = brickScale;
+                bricks.Add(newBrick);
+                blockXCoord += (blockWidth + blockPadding);
+            }
+            i++;
+        }
     }
 }
